b261935e73e1ae5a35daad0536b1f800f155e153
Moses <shroomce@gmail.com>
2020-12-23 13:54:25 +0400

    Изменение этапа без использования паттерна. Куча принтов готова, дописать парочку. Затем придумать структуру, показывающую геморойность внесения новых фич.

--- 01_template_game_publisher/01_without_template_pattern.py
+++ 01_template_game_publisher/01_without_template_pattern.py
@@ -3,62 +3,43 @@
 # Cеттинг - разные подходы к разработке игр у разных компаний
 
 
-class ElectronicArts:
-
-    def __init__(self):
-        self.game_in_development = None
-
-    def start_development(self, game_name):
-        self.game_in_development = game_name
-        self.gather_developer_team()
-        print('Начинаем разработку.')
-        self.improve_graphics()
-        print('Продолжаем разработку.')
-        self.add_more_explosions()
-        self.release()
-
-    def gather_developer_team(self):
+def create_game_by_company(game_name, company_name):
+    if company_name == 'Electronic Arts' or 'Ubisoft':  # developer_team
         print('Собираем команду разработчиков...')
+    elif company_name == 'Some_Indie_Developer':
+        print('Команда? Да я один справлюсь.')
+    else:
+        print(f'Генеральный директор "{company_name}" решает пустить игру на опенсорс. Игру разрабатывают индусы.')
 
-    def improve_graphics(self):
-        print('Улучшаем графоний...')
+    print(f'Разработка {game_name} началась.')  # development_started
 
-    def add_more_explosions(self):
-        print('Добавляем в игру больше взрывающихся объектов...')
+    if company_name == 'Electronic Arts':  # improvement over older renditions of the series
+        print('Больше зрелищных катсцен и взрывающихся вертолетов!')
+    elif company_name == 'Ubisoft':
+        print('Улучшаем графоний, а в остальном оставляем всё также - формула не подводит.')
+    elif company_name == 'Some_Indie_Developer':
+        print('У меня сейчас сессия, так что я отложу пока разработку.')
+    else:
+        print('Коллективное бессознательное индусов-опенсорсников насыщает игру задорной музыкой, танцами '
+              'и вулканами страстей.')
 
-    def release(self):
-        print(f'{self.game_in_development} готова.\n')
+    if company_name == 'Some_Indie_Developer': # development_continues
+        print('Отчислили. Ну да ладно, игрой займусь.')
+    else:
+        print(f'Разработка {game_name} продолжается.')
 
+    if company_name == 'Electronic Arts': # distinctive feature
+        print('"Ты уверен, что имеющихся взрывов и катсцен хватит? Может ещё добавим?"')
+    elif company_name == 'Ubisoft':
+        print('Разработка практически окончена, мир, как мы и хотели, открытый, '
+              'но не хватает последнего штриха - вышек.')
+    elif company_name == 'Some_Indie_Developer':
+        print('Сюжет игры будет разворачиваться вокруг непростой жизни антропоморфного горного барана Сергея,'
+              'страдающего сонными параличами. Каждый уровень будет одним из его снов, '
+              'раскрывающих завесу тайны его жизни.'
+              'Господи, да я же гениальный писатель, к черту этот вуз.')
+    else:
+        print('ТУТ_ВАРИАНТ_С_ИНДУСАМИ_КОТОРЫЙ_Я_ЕЩЁ_НЕ_ПРИДУМАЛ_')
 
-class Ubisoft:
+    print('РЕЛИЗ_ДОПИШУ_ПОЗЖЕ')
 
-    def __init__(self):
-        self.game_in_development = None
-
-    def start_development(self, game_name):
-        self.game_in_development = game_name
-        self.gather_developer_team()
-        print('Начинаем разработку.')
-        self.improve_graphics()
-        print('Продолжаем разработку.')
-        self.add_more_towers()
-        self.release()
-
-    def gather_developer_team(self):
-        print('Собираем команду разработчиков...')
-
-    def improve_graphics(self):
-        print('Улучшаем графоний...')
-
-    def add_more_towers(self):
-        print('Добавляем в открытый мир игры побольше вышек, на которые можно забраться...')
-
-    def release(self):
-        print(f'{self.game_in_development} готова.\n')
-
-
-EA = ElectronicArts()
-EA.start_development(game_name='Battlefield')
-
-UB = Ubisoft()
-UB.start_development(game_name="Assassin's creed")
