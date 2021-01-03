554510ea02538c68517909b98a1957a0934a121b
Waddik <vadim.shandrinoff@moex.com>
2021-01-03 14:17:42 +0300

    WIP: после разговора 03.01.21

--- 01_template_game_publisher/01_without_template_pattern.py
+++ 01_template_game_publisher/01_without_template_pattern.py
@@ -1,48 +1,67 @@
 
 # Пример ситуации, в которой шаблонный паттерн ещё не применён.
 # Cеттинг - разные подходы к разработке игр у разных компаний
+import random
+import time
 
 from tqdm import tqdm
 
-# def create_game_by_company(game_name, company_name):
-#     if company_name == 'Electronic Arts' or 'Ubisoft':  # developer_team
-#         print('Собираем команду разработчиков...')
-#     elif company_name == 'Some_Indie_Developer':
-#         print('Команда? Да я один справлюсь.')
-#     else:
-#         print(f'Генеральный директор "{company_name}" решает пустить игру на опенсорс. Игру разрабатывают индусы.')
-#
-#     print(f'Разработка {game_name} началась.')  # development_started
-#
-#     if company_name == 'Electronic Arts':  # improvement over older renditions of the series
-#         print('Больше зрелищных катсцен и взрывающихся вертолетов!')
-#     elif company_name == 'Ubisoft':
-#         print('Улучшаем графоний, а в остальном оставляем всё также - формула не подводит.')
-#     elif company_name == 'Some_Indie_Developer':
-#         print('У меня сейчас сессия, так что я отложу пока разработку.')
-#     else:
-#         print('Коллективное бессознательное индусов-опенсорсников насыщает игру задорной музыкой, танцами '
-#               'и вулканами страстей.')
-#
-#     if company_name == 'Some_Indie_Developer': # development_continues
-#         print('Отчислили. Ну да ладно, игрой займусь.')
-#     else:
-#         print(f'Разработка {game_name} продолжается.')
-#
-#     if company_name == 'Electronic Arts': # distinctive feature
-#         print('"Ты уверен, что имеющихся взрывов и катсцен хватит? Может ещё добавим?"')
-#     elif company_name == 'Ubisoft':
-#         print('Разработка практически окончена, мир, как мы и хотели, открытый, '
-#               'но не хватает последнего штриха - вышек.')
-#     elif company_name == 'Some_Indie_Developer':
-#         print('Сюжет игры будет разворачиваться вокруг непростой жизни антропоморфного горного барана Сергея,'
-#               'страдающего сонными параличами. Каждый уровень будет одним из его снов, '
-#               'раскрывающих завесу тайны его жизни.'
-#               'Господи, да я же гениальный писатель, к черту этот вуз.')
-#     else:
-#         print('ТУТ_ВАРИАНТ_С_ИНДУСАМИ_КОТОРЫЙ_Я_ЕЩЁ_НЕ_ПРИДУМАЛ_')
-#
-#     print('РЕЛИЗ_ДОПИШУ_ПОЗЖЕ')
+def gather_dev_team(company_name):
+    print(f'{company_name}: Собираем команду разработчиков...')
+    for i in tqdm(['11', '22', '33', ]):
+        print(i)
+        time.sleep(.5)
+    return random.randint(5, 100)
+
+
+def sigle_uno_dev(company_name):
+    print(f'Генеральный директор "{company_name}" решает пустить игру на опенсорс. Игру разрабатывают индусы.')
+    return random.randint(1000, 10000)
+
+
+def create_game_by_company(game_name, company_name):
+    if company_name == 'Electronic Arts' or 'Ubisoft':  # developer_team
+        num_staff = gather_dev_team(company_name)
+    elif company_name == 'Some_Indie_Developer':
+        print('Команда? Да я один справлюсь.')
+        num_staff = 1
+    else:
+        num_staff = sigle_uno_dev(company_name)
+
+    print(f'Разработка {game_name} началась.')  # development_started
+
+    if company_name == 'Electronic Arts':  # improvement over older renditions of the series
+        if num_staff < 10:
+            print('И так сойдет')
+        else:
+            print('Больше зрелищных катсцен и взрывающихся вертолетов!')
+    elif company_name == 'Ubisoft':
+        print('Улучшаем графоний, а в остальном оставляем всё также - формула не подводит.')
+    elif company_name == 'Some_Indie_Developer':
+        print('У меня сейчас сессия, так что я отложу пока разработку.')
+    else:
+        print('Коллективное бессознательное индусов-опенсорсников насыщает игру задорной музыкой, танцами '
+              'и вулканами страстей.')
+
+    if company_name == 'Some_Indie_Developer': # development_continues
+        print('Отчислили. Ну да ладно, игрой займусь.')
+    else:
+        print(f'Разработка {game_name} продолжается.')
+
+    if company_name == 'Electronic Arts': # distinctive feature
+        print('"Ты уверен, что имеющихся взрывов и катсцен хватит? Может ещё добавим?"')
+    elif company_name == 'Ubisoft':
+        print('Разработка практически окончена, мир, как мы и хотели, открытый, '
+              'но не хватает последнего штриха - вышек.')
+    elif company_name == 'Some_Indie_Developer':
+        print('Сюжет игры будет разворачиваться вокруг непростой жизни антропоморфного горного барана Сергея,'
+              'страдающего сонными параличами. Каждый уровень будет одним из его снов, '
+              'раскрывающих завесу тайны его жизни.'
+              'Господи, да я же гениальный писатель, к черту этот вуз.')
+    else:
+        print('ТУТ_ВАРИАНТ_С_ИНДУСАМИ_КОТОРЫЙ_Я_ЕЩЁ_НЕ_ПРИДУМАЛ_')
+
+    print('РЕЛИЗ_ДОПИШУ_ПОЗЖЕ')
 
 
 def electronic_arts(game_name):
@@ -99,5 +118,7 @@ def ubisoft(game_name):
     run()
 
 
-electronic_arts('Battlefield')
-ubisoft('assasins creed')
+# electronic_arts('Battlefield')
+# ubisoft('assasins creed')
+
+gather_dev_team('zx')
